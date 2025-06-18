f24ae42ad7911ccb017271943ba9c2c916b42d94
umayermdemon <mamudmdemon@gmail.com>
2025-06-18 17:03:25 +0600

    feat: new migration

--- generated/prisma/index.js
+++ generated/prisma/index.js
@@ -237,7 +237,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "C:\\Level-2\\Assignment\\Sql-Path\\B4A9-Idea-Hub\\Green-Circle-Server\\generated\\prisma",
+      "value": "C:\\Level-2\\Job Placement Assignment\\Pollished Greennovate Hub\\Greenovate-Hub-Server\\generated\\prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -251,7 +251,7 @@ const config = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "C:\\Level-2\\Assignment\\Sql-Path\\B4A9-Idea-Hub\\Green-Circle-Server\\prisma\\schema.prisma",
+    "sourceFilePath": "C:\\Level-2\\Job Placement Assignment\\Pollished Greennovate Hub\\Greenovate-Hub-Server\\prisma\\schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
