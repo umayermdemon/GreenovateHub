f9ff19ce4d4cacab001d4eb1121dc43a1ccbab0e
umayermdemon <mamudmdemon@gmail.com>
2025-09-06 19:30:25 +0600

    fix: responsive blog page for small device

---
+++ Client/src/app/(WithCommonLayout)/about/loading.tsx
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <div>
+      <h1>This is loading component</h1>
+    </div>
+  );
+};
+
+export default loading;
--- Client/src/app/(WithCommonLayout)/about/page.tsx
+++ Client/src/app/(WithCommonLayout)/about/page.tsx
@@ -1,12 +1,7 @@
 import AboutPage from "@/components/modules/About/AboutPage";
-import { Suspense } from "react";
 
 const About = () => {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <AboutPage />
-    </Suspense>
-  );
+  return <AboutPage />;
 };
 
 export default About;
